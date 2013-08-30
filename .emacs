d57251087b0aff7ab9fa974e9afb22f49bfbbf5a
llllllllll <joejev@gmail.com>
2013-08-29 21:52:41 -0400

    for laptop

--- .emacs
+++ .emacs
@@ -1,14 +1,13 @@
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(add-hook (quote haskell-mode-hook) t)
  '(column-number-mode t)
  '(custom-enabled-themes (quote (monokai)))
  '(custom-safe-themes (quote ("68769179097d800e415631967544f8b2001dae07972939446e21438b1010748c" "301f218fa2357b2aa2a433e049f87e059c5ba2ad8b161634d758fbf007bf1d0a" default)))
  '(global-linum-mode t)
- '(haskell-mode-hook (quote (turn-on-haskell-indent turn-on-haskell-indentation)))
+ '(haskell-font-lock-symbols nil)
+ '(haskell-indent-after-keywords (quote (("where" 4 0) ("of" 4) ("do" 4) ("mdo" 4) ("rec" 4) ("in" 4 0) ("{" 4) "if" "then" "else" "let")))
+ '(haskell-indent-thenelse 2)
+ '(haskell-mode-hook (quote (turn-on-haskell-indent)))
  '(if window-system t)
  '(inhibit-startup-screen t)
  '(initial-scratch-message ";; Scratch Buffer")
@@ -16,8 +15,21 @@
  '(show-paren-mode t)
  '(tool-bar-mode nil))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(default ((t (:family "Droid Sans Mono" :foundry "unknown" :slant normal :weight normal :height 113 :width normal)))))
+
+(require 'package)
+(add-to-list 'package-archives 
+    '("marmalade" .
+      "http://marmalade-repo.org/packages/"))
+(package-initialize)
+(global-rainbow-delimiters-mode)
+(global-set-key (kbd "M-<tab>") 'dabbrev-expand)
+(define-key minibuffer-local-map (kbd "M-<tab>") 'dabbrev-expand)
+
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups"))
+  backup-by-copying t
+  version-control t
+  delete-old-versions t
+  kept-new-versions 20
+  kept-old-versions 5
+  )
--- xmonad.hs
+++ xmonad.hs
@@ -11,40 +11,38 @@ import XMonad.Util.EZConfig
 import System.IO
 
 main = do
-  xmobar_in <- spawnPipe "xmobar" 
-  xmproc <- spawnPipe "xcompmgr" >> spawn "sh ~/.fehbg" 
+    xmobar_in <- spawnPipe "xmobar" 
+    xmproc <- spawn "xcompmgr" >> spawn "sh ~/.fehbg" 
            -- spawnPipe "sh ~/Documents/mouse.sh"
-  xmonad $ defaultConfig
-    { layoutHook         = smartBorders $ avoidStruts $ layoutHook defaultConfig
-    , terminal           = "gnome-terminal"
-    , borderWidth        = 0
-    , modMask            = mod4Mask
-    , manageHook         = manage_hooks
-    , logHook            = log_hooks xmobar_in
+    xmonad $ defaultConfig
+       { layoutHook         = smartBorders $ avoidStruts $ layoutHook defaultConfig
+       , terminal           = "gnome-terminal"
+       , borderWidth        = 0
+       , modMask            = mod4Mask
+       , manageHook         = manage_hooks
+       , logHook            = log_hooks xmobar_in
                            
-    } `additionalKeys`
-    [ ((mod4Mask .|. shiftMask, xK_z), spawn "gnome-screensaver-command --lock") 
-    , ((0,xK_Print), spawn "gnome-screenshot")
-    , ((mod4Mask,xK_F2), spawn "amixer set Master 2- unmute")
-    , ((mod4Mask,xK_F3), spawn "amixer set Master 2+ unmute")
-    , ((mod4Mask,xK_F1), spawn "amixer set Master toggle")
-    ]
+    } `additionalKeys` [ ((0,xK_Print), spawn "gnome-screenshot")
+                       , ((mod4Mask,xK_F2), spawn "amixer set Master 1- unmute")
+                       , ((mod4Mask,xK_F3), spawn "amixer set Master 1+ unmute")
+                       , ((mod4Mask,xK_F1), spawn "amixer set Master toggle")
+                       ]
 
 manage_hooks = composeAll
-                [ isFullscreen --> doFullFloat
-                , manageDocks <+> manageHook defaultConfig
-                ]
+               [ isFullscreen --> doFullFloat
+               , manageDocks <+> manageHook defaultConfig
+               ]
 
 log_hooks xmobar = composeAll
                    [ fadeInactiveLogHook 0.65
                    , dynamicLogWithPP
-                     xmobarPP{ ppOutput  = hPutStrLn xmobar
-                             , ppTitle   =xmobarColor "#00B7EB" "". shorten 50
-                             , ppLayout  = const ""
-                             , ppCurrent = xmobarColor "#DB2929" ""
-                             , ppHidden  = xmobarColor "#00B7EB" ""
-                             , ppSep     = " | "
-                             }
+                     xmobarPP { ppOutput  = hPutStrLn xmobar
+                              , ppTitle   = xmobarColor "#00B7EB" "" . shorten 50
+                              , ppLayout  = const ""
+                              , ppCurrent = xmobarColor "#DB2929" ""
+                              , ppHidden  = xmobarColor "#00B7EB" ""
+                              , ppSep     = " | "
+                              }
                    ]
 
 
