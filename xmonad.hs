b5e7d5b0b8eb1bdee718fcfcdf6bf6b9fb7c26c8
llllllllll <joejev@gmail.com>
2014-04-06 07:12:00 -0400

    xmonad

--- xmonad.hs
+++ xmonad.hs
@@ -1,48 +1,58 @@
 -- Joe Jevnik
--- 24.11.2013
+-- 2014.4.6
 
 import XMonad
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.FadeInactive
-import XMonad.Hooks.FadeWindows
-import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.ManageHelpers
-import XMonad.Hooks.SetWMName
-import XMonad.Layout.NoBorders
-import XMonad.Util.EZConfig
-import XMonad.Util.Run
+import XMonad.Hooks.DynamicLog    ( xmobarColor,shorten,xmobarPP,ppSep,ppTitle
+                                  , ppCurrent,ppHidden,ppOutput,ppLayout
+                                  , dynamicLogWithPP)
+import XMonad.Hooks.FadeInactive  (fadeInactiveLogHook)
+import XMonad.Hooks.ManageDocks   (manageDocks,avoidStruts)
+import XMonad.Hooks.ManageHelpers (isFullscreen,doFullFloat)
+import XMonad.Hooks.SetWMName     (setWMName)
+import XMonad.Layout.NoBorders    (smartBorders)
+import XMonad.Util.EZConfig       (additionalKeys,parseKey)
+import XMonad.Util.Run            (spawnPipe)
 
+import Data.Monoid                (Endo)
+import System.IO                  (Handle,hPutStrLn)
+
+main :: IO ()
 main = do
-    xmobar_in <- spawnPipe "xmobar"
+    xmobarIn <- spawnPipe "xmobar"
     mapM_ spawn ["xcompmgr","sh ~/.fehbg","xrdb ~/.Xresources"]
     xmonad $ defaultConfig
        { layoutHook  = smartBorders $ avoidStruts $ layoutHook defaultConfig
        , terminal    = "xterm"
        , borderWidth = 0
        , modMask     = mod4Mask
-       , manageHook  = manage_hooks
-       , logHook     = log_hooks xmobar_in
+       , manageHook  = manageHooks
+       , logHook     = logHooks xmobarIn
        , startupHook = setWMName "XMonad"
-    } `additionalKeys` [ ((0,xK_Print), spawn "gnome-screenshot")
-                       , ((mod4Mask,xK_F2), spawn "amixer set Master 1- unmute")
-                       , ((mod4Mask,xK_F3), spawn "amixer set Master 1+ unmute")
-                       , ((mod4Mask,xK_F1), spawn "amixer set Master toggle")
-                       ]
+       } `additionalKeys` addedKeys
 
-manage_hooks = composeAll
-               [ isFullscreen --> doFullFloat
-               , manageDocks <+> manageHook defaultConfig
-               ]
+addedKeys :: [((ButtonMask, KeySym), X ())]
+addedKeys = [ ((0,xK_Print),        spawn "gnome-screenshot")
+            , ((mod4Mask,xK_F2),    spawn "amixer set Master 1- unmute")
+            , ((mod4Mask,xK_F3),    spawn "amixer set Master 1+ unmute")
+            , ((mod4Mask,xK_grave), spawn "strp -c")
+            ]
 
-log_hooks xmobar = composeAll
-                   [ fadeInactiveLogHook 0.65
-                   , dynamicLogWithPP
-                     xmobarPP { ppOutput  = hPutStrLn xmobar
-                              , ppTitle   = xmobarColor "#00B7EB" ""
-                                            . shorten 50
-                              , ppLayout  = const ""
-                              , ppCurrent = xmobarColor "#DB2929" ""
-                              , ppHidden  = xmobarColor "#00B7EB" ""
-                              , ppSep     = " | "
-                              }
-                   ]
+manageHooks :: Query (Endo WindowSet)
+manageHooks = composeAll
+              [ isFullscreen --> doFullFloat
+              , manageDocks <+> manageHook defaultConfig
+              ]
+
+logHooks :: Handle -> X ()
+logHooks xmobar = composeAll
+                  [ fadeInactiveLogHook 0.65
+                  , dynamicLogWithPP
+                    xmobarPP { ppOutput  = hPutStrLn xmobar
+                             , ppTitle   = xmobarColor "#00B7EB" ""
+                                           . shorten 50
+                             , ppLayout  = const ""
+                             , ppCurrent = xmobarColor "#DB2929" ""
+                             , ppHidden  = xmobarColor "#00B7EB" ""
+                             , ppSep     = " | "
+                             }
+                  ]
